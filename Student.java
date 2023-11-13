53983f68a0795262bdc01a66ec0f65052e7c9fd4
Anastasia1921 <150111215+Anastasia1921@users.noreply.github.com>
2023-11-13 11:30:27 +0300

    Add files via upload

--- Student.java
+++ Student.java
@@ -8,9 +8,9 @@ public class Student implements Learner {
     @Override
     public void learn() {
         Date current = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.YYYY HH:mm");
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
         String message = formatter.format(current);
-        System.out.printf("Дата-время начала учёбы: %s \n", message);
+        System.out.printf("Время начала учёбы: %s \n", message);
         System.out.println("Я учусь. .zZ");
         IntStream.range(0, 1000000).forEach(i -> Math.log(i));
         System.out.println("Я закончил учиться");
